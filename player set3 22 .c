952a78c8dc4b44f54107b7d8d124b7f41737e1cc
kowsalyavsb <31506020+kowsalyavsb@users.noreply.github.com>
2018-03-02 16:49:05 +0530

    Update player set3 22 .c

--- player set3 22 .c
+++ player set3 22 .c
@@ -1,1 +1,16 @@
 
+#include <stdio.h>
+
+int main(void) {
+	int a,b,c,i;
+	scanf("%d %d",&a,&b);
+	for(i=2;i<=a;i++)
+	{
+		if(a%i==0 && b%i==0)
+		{
+			c=i;
+		}
+	}
+	printf("%d",c);
+	return 0;
+}
